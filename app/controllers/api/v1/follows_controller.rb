c483430f497f2341b358076d9580910b328a71ca
UncleSam <shrimp509@hotmail.com>
2022-12-15 22:15:22 +0800

    Refactor follow APIs and Add follow validations

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -3,16 +3,17 @@ class Api::V1::FollowsController < ApplicationController
 
   def create
     followed = User.find_by(id: params[:follow_id])
-    return error_response(400, 'User has been followed') if current_user.followeds.include?(followed)
-    current_user.followeds << followed
+    return error_response(403, 'User has been followed') if current_user.followeds.include?(followed)
+    result = current_user.follow(followed)
+    return error_response(500, 'Follow failed for unknown reason') unless result
     success_response('Follow success')
   end
 
   def unfollows
     unfollowed = User.find_by(id: params[:unfollow_id])
-    follow_record = current_user.follows.find_by(followed: unfollowed)
-    return error_response(400, 'User is not followed') unless follow_record
-    follow_record.destroy
+    return error_response(403, 'User is not followed') unless current_user.followeds.include?(unfollowed)
+    result = current_user.unfollow(unfollowed)
+    return error_response(500, 'Unfollow failed for unknown reason') unless result
     success_response('Unfollow success')
   end
 
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,17 @@
 class Follow < ApplicationRecord
   belongs_to :followed, class_name: 'User', foreign_key: :followed_id
   belongs_to :follower, class_name: 'User', foreign_key: :follower_id
+
+  validate :already_followed, on: [:create, :update]
+  validate :follow_self, on: [:create, :update]
+
+  private
+
+  def already_followed
+    errors.add(:base, :invalid, message: 'Already followed') if Follow.find_by(followed: followed, follower: follower)
+  end
+
+  def follow_self
+    errors.add(:base, :invalid, message: 'Cannot follow yourself') if followed.id == follower.id
+  end
 end
